fc7539aa05a8e2eb24a06879019323837baf0ebf
lattepapa <whitelatte46@gmail.com>
2022-01-07 22:43:30 +0900

    Initial Commit

---
+++ Quotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Quotes
+//
+//  Created by Siwoo Baek on 2022/01/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
